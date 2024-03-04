1cd016680a1e112762b84ea333940d213f278d99
atsogt <atsogt17@gmail.com>
2024-03-03 19:51:31 -0500

    process env fixed

--- index.js
+++ index.js
@@ -22,11 +22,9 @@ const port = process.env.PORT || 3000;
 
 const connectToDB = async () => {
   try {
-    await mongoose
-      .connect("mongodb://localhost:27017/task-manager-api")
-      .then(() => {
-        console.log("Connected to database");
-      });
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
+      console.log("Connected to database");
+    });
   } catch (e) {
     throw new Error(`Error connecting to the database: ${e.message}`);
   }
