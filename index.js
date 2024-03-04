26c679b8fbfc1df9293403c8e102250b58a8c6b3
atsogt <atsogt17@gmail.com>
2024-03-03 19:49:12 -0500

    test connection succeeded

--- index.js
+++ index.js
@@ -4,31 +4,45 @@ import "dotenv/config";
 import { MongoClient } from "mongodb";
 import User from "./src/models/user.js";
 import mongoose from "mongoose";
+import e from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
-const client = new MongoClient(process.env.MONGO_URI);
-const database = client.db("users-test");
-const userCollection = database.collection("users");
-client
-  .connect()
-  .then(() => {
-    console.log("connected to mongodb");
-  })
-  .catch((error) => console.log("Not Connected to mongoDB", error));
+// const client = new MongoClient(process.env.MONGO_URI);
+// const database = client.db("task-manager-api");
+// const userCollection = database.collection("users");
+// client
+//   .connect()
+//   .then(() => {
+//     console.log("connected to mongodb");
+//   })
+//   .catch((error) => console.log("Not Connected to mongoDB", error));
+
+const connectToDB = async () => {
+  try {
+    await mongoose
+      .connect("mongodb://localhost:27017/task-manager-api")
+      .then(() => {
+        console.log("Connected to database");
+      });
+  } catch (e) {
+    throw new Error(`Error connecting to the database: ${e.message}`);
+  }
+};
+
+connectToDB();
 
 app.get("/", async (req, res) => {
-  const users = await userCollection.find().toArray();
+  const users = await User.find({});
   res.json(users);
 });
 
 app.post("/", async (req, res) => {
-  //
   const user = await new User(req.body);
   try {
-    await userCollection.insertOne(user);
+    await user.save();
     res.status(200).send({ user });
   } catch (error) {
     res.status(400).send("Not able to post user nor send email");
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,67 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
+import { mongoose, Schema } from "mongoose";
+// const { Schema } = mongoose;
+import validator from "validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
 // const Task = require("./task");
 
-const userSchema = new Schema({
-  name: { type: String },
-  email: { type: String },
-  password: { type: String },
-  age: { type: Number },
-});
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error('Password cannot contain "password"');
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a postive number");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          require: true,
+        },
+      },
+    ],
+    avatar: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
