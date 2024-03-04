386cb9bf225a732cb74f1f1b651ba7b011284a55
atsogt <atsogt17@gmail.com>
2024-03-04 10:36:57 -0500

    user router modified

---
+++ src/emails/account.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+let config = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+};
+const transporter = nodemailer.createTransport(config);
+
+let sendWelcomeEmail = async (email, name) => {
+  let message = {
+    subject: "Thank you for signing up!", // Subject line
+    from: process.env.EMAIL, // sender address
+    to: email, // list of receivers
+    text: `Welcome to the app, ${name}! Hope you enjoy your own personal task app.`,
+  };
+  await transporter.sendMail(message);
+};
+
+let sendCancelationEmail = async (email, name) => {
+  let message = {
+    subject: "You're account is cancelled!", // Subject line
+    from: process.env.EMAIL, // sender address
+    to: email, // list of receivers
+    text: `We're sorry to see you go, ${name}! Please tell use why you cancelled your account.`, // html body
+  };
+  await transporter.sendMail(message);
+};
+
+module.exports = { sendWelcomeEmail, sendCancelationEmail };
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,21 @@ const User = require("../models/user");
 // import { auth } from "../middleware/auth.js";
 const auth = require("../middleware/auth");
 const router = new express.Router();
+const multer = require("multer");
+
+const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      cb(new Error("Please upload a image file"));
+    }
+    cb(undefined, true);
+  },
+});
 
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
@@ -22,11 +37,19 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
-router.post("/users/login", async (req, res) => {
-  console.log("Login Hit");
+router.post("/users", async (req, res) => {
+  const user = await new User(req.body);
   try {
-    // console.log(req.body.email);
-    // console.log(req.body.password);
+    await user.save();
+    sendWelcomeEmail(user.email, user.name);
+    res.status(201).send({ user });
+  } catch (error) {
+    res.status(400).send("Not able to post user nor send email");
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
@@ -39,13 +62,100 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.post("/user", async (req, res) => {
-  const user = await new User(req.body);
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    await user.save();
-    res.status(200).send({ user });
+    //filetering out token
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
   } catch (error) {
-    res.status(400).send("Not able to post user nor send email");
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res
+      .status(200)
+      .send("All tokens deleted. User is logged out of every device");
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ Error: error.message });
+  }
+);
+
+router.patch("/users/me", auth, async (req, res) => {
+  //grabs keys
+  const updates = Object.keys(req.body);
+  // console.log(updates);
+  const allowedUpdates = ["name", "email", "age", "password"];
+  const isValidUpdate = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidUpdate) {
+    return res.status(406).send({ updateMessage: "Update not allowed" });
+  }
+
+  try {
+    const user = req.user;
+    //dynamic update
+    updates.forEach((update) => {
+      user[update] = req.body[update];
+    });
+
+    user.save();
+
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete(
+  "/users/me/avatar",
+  auth,
+  async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.remove();
+    res.send("Deleted profile avatar");
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ Error: error.message });
+  }
+);
+
+router.delete("/users/me", auth, async (req, res) => {
+  const user = req.user;
+  console.log("hit");
+  console.log(user);
+  try {
+    sendCancelationEmail(user.email, user.name);
+    await req.user.deleteOne({ _id: user._id });
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
